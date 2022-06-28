c6f6eb69e410712d649f9bc79296ed129ed975b0
dupeng <dupeng@mxnavi.com>
2022-06-28 14:12:40 +0800

    上传代码

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { watchAtMost  } from '@vueuse/core'
+import {ref} from 'vue'
+
+const source = ref(0)
+
+watchAtMost(
+  source,
+  (value) => {
+    console.log('变化了', value)
+  },
+  {
+    count: 4
+  }
+)
+
+const clickedFn = () => {
+  source.value ++
+}
+
+</script>
+
+<template>
+ <div>{{source}}</div>
+  <button @click="clickedFn">
+    点击按钮
+  </button>
+</template>
